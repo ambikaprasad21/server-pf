38375b95c0ef113020d7d607c1c0524bbb1fb3a5
Ambika Prasad <akashboard2021@gmail.com>
2023-11-04 03:39:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Replace these with your email credentials
+const emailUser = process.env.EMAIL_USER;
+const emailPass = process.env.EMAIL_PASSWORD;
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: emailUser,
+    pass: emailPass,
+  },
+});
+
+app.post("/submit-form", (req, res) => {
+  const { fullName, email, message } = req.body;
+
+  const mailOptions = {
+    from: emailUser,
+    to: "your-email@example.com",
+    subject: "New Form Submission",
+    text: `Full Name: ${fullName}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
